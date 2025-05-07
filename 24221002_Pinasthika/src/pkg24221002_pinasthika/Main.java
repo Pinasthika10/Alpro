543946c3bdfca6a5e42c51f5042e7bc3549c21b8
Pinasthika10 <eshapinasthika16@gmail.com>
2025-05-07 15:08:53 +0700

    Add files via upload
    Alpro

---
+++ 24221002_Pinasthika/src/pkg24221002_pinasthika/Main.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg24221002_pinasthika;
+
+/**
+ *
+ * @author ACER
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     int nim = 24221002;
+     String nama = "Pinasthika";
+     String alamat = "Kayuhan Wetan, Kab Bantul, Kec Pajangan";
+     String TTL = "Bantul, 16 Oktober 2005";
+     
+     
+     
+        System.out.println("biodata");
+        
+         System.out.println("NIM = "+nim);
+          System.out.println("NAMA = "+nama);
+          System.out.println("Alamat = "+alamat);
+          System.out.println("Tempat Tanggal Lahir = "+TTL);
+    }
+    
+}
